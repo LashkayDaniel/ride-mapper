f8102b566f78fff707e0fe056fde04eefd91ec60
lashkaydaniel <lashkay74y@gmail.com>
2024-09-20 14:52:58 +0300

    chore: project init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="w-full h-screen flex flex-col">
+    <div class="h-full">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
