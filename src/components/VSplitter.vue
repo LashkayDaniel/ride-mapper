9778ecb5e2d78e1bf4e31ce8d30c8d651cf91529
lashkaydaniel <lashkay74y@gmail.com>
2024-09-26 19:13:54 +0300

    feat: create VSplitter.vue

---
+++ src/components/VSplitter.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex items-center my-4">
+    <hr class="flex-1">
+    <h2 class="px-4 font-semibold text-gray-500">
+      <slot></slot>
+    </h2>
+    <hr class="flex-1">
+  </div>
+</template>
