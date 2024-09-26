1712b7dcf6240cf83be44c7aa06a6af167f7d818
lashkaydaniel <lashkay74y@gmail.com>
2024-09-26 19:13:39 +0300

    feat: create VButton.vue

---
+++ src/components/VButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  type?: 'button' | 'submit' | 'reset';
+}
+
+withDefaults(defineProps<Props>(), { type: 'button' })
+const emit = defineEmits(['click'])
+</script>
+
+<template>
+  <button
+    :type="type"
+    class="bg-black/90 text-white px-4 py-1 rounded-full hover:bg-black/80 transition-colors"
+    @click="emit('click')"
+  >
+    <slot />
+  </button>
+</template>
