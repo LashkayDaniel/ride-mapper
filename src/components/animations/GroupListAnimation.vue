45d432b0f975908039cc21f9dfe8b86348dfcd73
lashkaydaniel <lashkay74y@gmail.com>
2024-09-26 19:15:07 +0300

    feat: create animation for lists item

---
+++ src/components/animations/GroupListAnimation.vue
@@ -0,0 +1,18 @@
+<template>
+  <TransitionGroup name="list" tag="ul">
+    <slot />
+  </TransitionGroup>
+</template>
+
+<style scoped>
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
