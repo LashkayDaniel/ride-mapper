6f82cccc8bbf712008437199ee07331abf36e57e
lashkaydaniel <lashkay74y@gmail.com>
2024-09-26 19:13:14 +0300

    feat: create & connect TheHeader.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import TheHeader from '@/components/TheHeader.vue'
 </script>
 
 <template>
   <div class="w-full h-screen flex flex-col">
-    <div class="h-full">
+    <TheHeader />
+    <div class="w-full flex-1 overflow-auto">
       <RouterView />
     </div>
   </div>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="w-full flex justify-center px-10 py-4 items-center bg-gray-300">
+    <img class="size-12" src="@/assets/logo.svg" alt="logo">
+    <h1 class="font-bold text-xl">Ride Mapper</h1>
+  </header>
+</template>
