edd5740527110869aeffa482e2dc05e84db315fb
lashkaydaniel <lashkay74y@gmail.com>
2024-10-05 20:05:58 +0300

    refactor: update header logo

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 
+import IconLogo from '@/components/icons/IconLogo.vue'
 </script>
 
 <template>
-  <header class="w-full flex justify-center px-10 py-4 items-center bg-gray-300">
-    <img class="size-12" src="@/assets/logo.svg" alt="logo">
+  <header
+    class="w-full flex justify-center px-10 py-4 items-center bg-black text-gray-300 gap-x-2">
+    <IconLogo />
     <h1 class="font-bold text-xl">Ride Mapper</h1>
   </header>
 </template>
